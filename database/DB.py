deae2f1aaafe0411805e940a68f4e2730e815246
ChiInCheang <34171576+ChiInCheang@users.noreply.github.com>
2020-03-30 11:12:04 +0800

    add getPrereq function

--- database/DB.py
+++ database/DB.py
@@ -224,6 +224,7 @@ class DB():
         result = self.cur.fetchall()
         self.cur.close()
         return result
+
     # return the count of lesson for each crn
     def getLessonCount(self, subj, crse):
         self.cur = self.db.cursor()
@@ -232,6 +233,17 @@ class DB():
         var = (subj, crse)
         self.cur.execute(sql, var)
         result = self.cur.fetchall()
+        self.cur.close()
+        return result
+
+    # return prerequisites of course
+    def getPrereq(self, subj, crse):
+        self.cur = self.db.cursor()
+        sql = "select prereq1, prereq2, prereq3 from classes where subj = %s and crse = %s"
+        var = (subj, crse)
+        self.cur.execute(sql, var)
+        result = self.cur.fetchall()
+        result = result[0]
         self.cur.close()
         return result
 
