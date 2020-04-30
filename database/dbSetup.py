8e1b2b48a87e4095930fe76ed152d997baf2f02d
ChiInCheang <34171576+ChiInCheang@users.noreply.github.com>
2020-04-30 23:44:54 +0800

    auto read .csv file

--- database/dbSetup.py
+++ database/dbSetup.py
@@ -1,18 +1,18 @@
 from core.Course import *
-
+import os
 
 if __name__ == '__main__':
 
-    csFile = 'cs.csv'
-    langFile = 'lang.csv'
-    rootsFile = 'roots.csv'
-    mapFile = 'map.csv'
+    dir = os.getcwd()   # current directory
+    print(dir)
+    fileList = os.listdir(dir)  # files in current directory
+    print(fileList)
 
     testdb = DB()
     flag = testdb.createDatabase()  # return 1 if database is newly created, 0 if database already created
-    testdb.importData(csFile, flag)
-    testdb.importData(langFile, flag)
-    testdb.importData(rootsFile, flag)
-    testdb.importData(mapFile, flag)
 
+    for file in fileList:
+        if file[-4:] == '.csv': # only read the .csv file
+            print(file)
+            testdb.importData(file, flag)
     testdb.close()
